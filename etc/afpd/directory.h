b9623cc2d036a0fb30d174facf16d935315ec388
didg <didg>
2003-05-10 10:33:16 +0000

    backport from HEAD

--- etc/afpd/directory.h
+++ etc/afpd/directory.h
@@ -1,5 +1,5 @@
 /*
- * $Id: directory.h,v 1.13.2.1 2003-04-28 10:12:53 didg Exp $
+ * $Id: directory.h,v 1.13.2.2 2003-05-10 10:33:16 didg Exp $
  *
  * Copyright (c) 1990,1991 Regents of The University of Michigan.
  * All Rights Reserved.
@@ -230,6 +230,9 @@ extern int	afp_mapname __P((AFPObj *, char *, int, char *, int *));
 extern int	afp_enumerate __P((AFPObj *, char *, unsigned int, char *, unsigned int *));
 extern int	afp_enumerate_ext __P((AFPObj *, char *, unsigned int, char *, unsigned int *));
 extern int	afp_enumerate_ext2 __P((AFPObj *, char *, unsigned int, char *, unsigned int *));
+
+/* from catsearch.c */
 extern int	afp_catsearch __P((AFPObj *, char *, int, char *, int *));
+extern int	afp_catsearch_ext __P((AFPObj *, char *, int, char *, int *));
 
 #endif
